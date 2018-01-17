6f68aab7c47b8be405113278df97116f0bf0ed21
jack-barbey <jack.barbey@gmail.com>
2018-01-17 15:08:17 -0600

    Uses salted hashing, including option to concatenate the hashes to save space

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # obscuritext
 A Python script to obscure free text into anonymized data by hashing each word. These hashes can still be used for statistical modeling but have no identifying information and are completely unreadable.
 
-## Requirements:
+## Requirements
 The text to be obscured should be stored in a named column in a CSV file. This file should be stored in the same directory as the script file.
 
 To run, use: python3 text_obscure.py
@@ -13,14 +13,15 @@ The script uses the following packages for Python3:
 
 
 ## Configurations
-- file_name: Specify the name of the CSV file without its extension:
-- output_base: Specify the base output name for the cleansed file:
-- column_name: Specify the name of the column to cleanse
-- delete_column: Would you like to delete that column in the output file? (Yes/No)
-- index_num: The number (starting with 0) of the index column. Write None is none exists
-- case_sensitive: Would you like to run as case sensitive? (Yes, No, or Both)
-- stemming: Would you like to use stemming, which requires importing NLTK? (Yes, No, or Both)
-- remove_punctuation: Would you like to remove punctuation (Yes, No, or Both)? Selecting 'No' will treat punctuation as words themselves.
-- random_seed: Please enter a seed integer for the randomizer
-- combine_above: Combine words with counts above (an integer, or, if you want to be shown the counts and then input a number in the terminal, write 'Ask')
+- file_name: The name of the CSV file without its extension:
+- output_base: The base output name for the cleansed file:
+- column_name: The name of the column to cleanse
+- delete_column: Delete the original column in the output file? (Yes/No)
+- index_num: The number (starting with 0) of the index column. 'None' if not present
+- case_sensitive: Run as case sensitive? (Yes, No, or Both)
+- stemming: Use stemming, which requires importing NLTK? (Yes, No, or Both)
+- remove_punctuation: Remove punctuation (Yes, No, or Both)? Selecting 'No' will treat punctuation as words themselves.
+- salt_string: A string to be used as a salt during hashing
+- concat_hashes: How many characters to which to concatenate the hashes. (Int or None)
+- combine_above: Combine words with counts above (an integer or 'Ask)
 - combine_below = 1: Combine words with counts below (an integer or 'Ask')
--- text_obscure.py
+++ text_obscure.py
@@ -5,107 +5,108 @@ import os, errno
 import configparser
 
 
-def get_nums(word_list, word_numbers, word_numbers_list, number):
+def get_hashes(word_list, word_hashes, word_hashes_list, salt, concat_hashes):
     '''
     Given a list of words from a csv cell, returns a string of obscured words.
     INPUTS:
         word_list (list): the original words
-        word_numbers (dict): contains parings of word -> number
-        word_numbers_list (list of lists): contains words, numbers, count (initialized to None)
+        word_hashes (dict): contains parings of word -> hash
+        word_hashes_list (list of lists): contains [[word1, hash1], [word2, hash2] ...]
+        concat_hashes (int or None): the number of digits to concat the hash
     OUTPUT:
-        return_string (string): the string of numbers to stand in for the original words
+        return_string (string): the string of hashes to stand in for the original words
     '''
     for word in word_list:
-        if word not in word_numbers:
-            # Dictionary of {word -> [number, count]}
-            word_numbers[word] = [number, 1]
-            # List of lists with the form [[word, number]]
-            word_numbers_list.append([word, number])
-            number += 1
+        if word not in word_hashes:
+            to_hash = word + salt
+            hasher = hashlib.sha1(to_hash.encode('utf-8'))
+            #Hashes using SHA1 then optionally truncates to prevent too long of hashes. Removes the padding at the end.
+            word_hash = base64.urlsafe_b64encode(hasher.digest()).decode("utf-8").replace("=", "")[:concat_hashes]
+
+            # Dictionary of {word -> [hash, count]}
+            word_hashes[word] = [word_hash, 1]
+            # List of lists with the form [[word, hash]]
+            word_hashes_list.append([word, word_hash])
         else:
             # Increases count by 1 if already in dictionary
-            word_numbers[word][1] += 1
+            word_hashes[word][1] += 1
 
-    return number
 
-def replace_words(word_list, word_numbers):
+def replace_words(word_list, word_hashes):
     '''
     Given a list of words from a csv cell, returns a string of obscured words.
     INPUTS:
         word_list (list): the unhased words
-        word_numbers (dict): contains parings of word -> number
-        word_numbers_list (list of lists): contains words, numbers
+        word_hashes (dict): contains parings of word -> hash
+        word_hashes_list (list of lists): contains [[word1, hash1], [word2, hash2] ...]
         bottom_thresh (int): only replace the word if count above this
         top_thresh (int): only replace the word if count below this
     OUTPUT:
-        return_string (string): the string of numbers to stand in for the original words
+        return_string (string): the string of hashes to stand in for the original words
     '''
     replace_string = ""
     for word in word_list:
-        # Retrieves the shuffled number for that word
-        word_number = word_numbers[word][0]
-        replace_string += str(word_number) + " "
+        # Retrieves the hash for that word
+        word_hash = word_hashes[word][0]
+        replace_string += str(word_hash) + " "
 
     return replace_string
 
 
-def assign_or_replace_text(original_text, punctuation, word_numbers, word_numbers_list, assign, ps, stemming, case_sensitive):
+def assign_or_replace_text(original_text, punctuation, word_hashes, word_hashes_list,
+                            assign, ps, stemming, case_sensitive, salt, concat_hashes):
     '''
     Loops through the original text. Depending on assign (bool), performs one of
-    two options. Either assigns ascending numbers to the unique words OR, after shuffling,
+    two options. Either assigns hashes to the unique words OR
     creates a new series of obscured text.
     INPUTS:
         original_text (Series): the column to be obscured
         punctuation (bool): if punctuation should be removed
-        word_numbers (dict): to be filled on first pass with word -> number representative
-            later to be used to replace words with shuffled numbers
-        word_numbers_list (list of lists): same info as above
-        assign (bool): tells whether to run to assign numbers or to retrieve numbers
+        word_hashes (dict): to be filled on first pass with word -> hash representative
+            later to be used to replace words with hashes
+        word_hashes_list (list of lists): same info as above
+        assign (bool): tells whether to run to assign hashes or to retrieve hashes
         ps: PorterStemmer or none
         case_sensitive (bool): indicates whether to be case_sensitive
 
     OUTPUTS:
-        Nothing on the first pass. Populates word_numbers and word_numbers_list
-        obscured_text (List of strings): the text with the shuffled numbers
+        Nothing on the first pass. Populates word_hashes and word_hashes_list
+        obscured_text (List of strings): the text with the hashes
     '''
     # A regex pattern for replacing all of the punctuation in one pass
     pattern = re.compile("(" + "[^A-Za-z0-9 ]+" + ")")
 
-    if assign:
-        number = 1 #Only used in the first pass
-    else:
+    if not assign:
         obscured_text = [] #Only used in the second pass
 
     for text in original_text:
-        # Only performs punctuation stripping and splitting if the field is a string
-        if type(text) is str:
-            if punctuation:
-                post_punc = pattern.sub(" ", text)
-            else:
-                # Isolates punctuation to be treated as an individual word
-                post_punc = pattern.sub(r' \1 ', text)
+        if type(text) is not "str":
+            text = str(text)
 
-            if case_sensitive:
-                words_list = post_punc.split()
-            else:
-                words_list = post_punc.lower().split()
-
-            if stemming:
-                words_list = [ps.stem(i) for i in words_list]
-        # If the field is not a string, simply sends the whole field to be numerated
+        if punctuation:
+            post_punc = pattern.sub(" ", text)
         else:
-            words_list = [text]
+            # Isolates punctuation to be treated as an individual word
+            post_punc = pattern.sub(r' \1 ', text)
+
+        if case_sensitive:
+            words_list = post_punc.split()
+        else:
+            words_list = post_punc.lower().split()
+
+        if stemming:
+            words_list = [ps.stem(i) for i in words_list]
 
         if assign:
-            number = get_nums(words_list, word_numbers, word_numbers_list, number)
+            get_hashes(words_list, word_hashes, word_hashes_list, salt, concat_hashes)
         else:
-            replace_string = replace_words(words_list, word_numbers)
+            replace_string = replace_words(words_list, word_hashes)
             obscured_text.append(replace_string)
 
     if not assign:
         return obscured_text
 
-def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_numbers_df):
+def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df):
     '''
     If top_thresh or bottom_thresh was set to "Ask" in the config file, shows the user the
     head or tail of the dataset and asks for the desired count.
@@ -117,7 +118,7 @@ def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_numbers_df):
         bottom_thresh (int): the desired bottom threshold, as an integer
     '''
     if top_thresh.lower() == "ask":
-        print(word_numbers_df.head(20),  "\n")
+        print(word_hashes_df.head(20),  "\n")
         top_thresh = input("Combine words with counts above: ")
     while True:
         try:
@@ -128,7 +129,7 @@ def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_numbers_df):
             top_thresh = input("Combine words with counts above: ")
 
     if bottom_thresh.lower() == "ask":
-        print("\n", word_numbers_df.tail(20), "\n")
+        print("\n", word_hashes_df.tail(20), "\n")
         bottom_thresh = input("Combine words with counts below: ")
     while True:
         try:
@@ -140,7 +141,7 @@ def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_numbers_df):
 
     return top_thresh, bottom_thresh
 
-def export_tables(csv_df, word_numbers_df, output_base, column_name, case_sensitive,
+def export_tables(csv_df, word_hashes_df, output_base, column_name, case_sensitive,
                     stemming, punctuation, top_thresh, bottom_thresh, index_num, path):
     '''
     Strings together the options to create a descriptive filename.
@@ -167,10 +168,10 @@ def export_tables(csv_df, word_numbers_df, output_base, column_name, case_sensit
 
 
     csv_df.to_csv(path + output_name + ".csv", index= not type(index_num) is int)
-    word_numbers_df.to_csv(path + "mappings_" + output_name + ".csv", index=False)
+    word_hashes_df.to_csv(path + "mappings_" + output_name + ".csv", index=False)
 
 def run_program(file_name, column_name, output_base, delete_orig, index_num, case_sensitive,
-                stemming, punctuation, seed, top_thresh, bottom_thresh):
+                stemming, punctuation, salt, top_thresh, bottom_thresh, concat_hashes):
     '''
     Given the configuration settings, runs the obscuration process. Can be called many times for different settings.
     Inputs:
@@ -181,73 +182,67 @@ def run_program(file_name, column_name, output_base, delete_orig, index_num, cas
         index_num (int or None): the number of the column in the original file with idecies
         case_sensitive (bool):
         stemming (bool): to use NLTK word stemming
-        seed (int): a random seed for the shuffling
+        salt (string): a user-inputed string for the salting the hashes
         punctuation (bool): to remove punctuation
         top_thresh ("Ask" or int): combine words above this count
         bottom_thresh ("Ask or int): combine words below this count
+        concat_hashes (int or None): number of bytes to concatenate the hashes
     Output:
-        Nothing, but exports a two files: one with the obscured text and the other mapping original_word -> number
+        Nothing, but exports a two files: one with the obscured text and the other mapping original_word -> hash
     '''
-
-    if stemming:
-        from nltk.stem import PorterStemmer
-        ps = PorterStemmer()
-    else:
-        ps = None
-
-    try:
-        index_num = int(index_num)
-    except ValueError:
-        index_num = None
-
-    #Contains a dictionary of original word -> [number of word, count]
-    word_numbers = {}
+    #Contains a dictionary of original word -> [hash of word, count]
+    word_hashes = {}
     #Contains the same information for export to csv (using list of lists for efficiency)
-    word_numbers_list = []
+    word_hashes_list = []
 
     csv_df = pd.read_csv(file_name + ".csv", encoding = "ISO-8859-1", index_col = index_num)
     original_text = csv_df[column_name]
 
-    #The first pass through assign_or_replace_text to assign ascending numbers
-    assign_or_replace_text(original_text, punctuation, word_numbers, word_numbers_list, True, ps, stemming, case_sensitive)
+    #The first pass through assign_or_replace_text to assign hashes
+    assign_or_replace_text(original_text, punctuation, word_hashes, word_hashes_list, True,
+                            ps, stemming, case_sensitive, salt, concat_hashes)
 
-    #Fetches the counts of the words, in order, to be filled into the word_numbers_df created below
+    #Fetches the counts of the words, in order, to be filled into the word_hashes_df created below
     counts = []
-    for word, number in word_numbers_list:
-        counts.append(word_numbers[word][1])
+    for word, word_hash in word_hashes_list:
+        counts.append(word_hashes[word][1])
 
 
     #Assigns the proper counts, and sorts by them
-    word_numbers_df = pd.DataFrame(word_numbers_list, columns=["Original_Word", "Number"])
-    word_numbers_df["Count"] = counts
-    word_numbers_df.sort_values(by = "Count", ascending=False, inplace=True)
+    word_hashes_df = pd.DataFrame(word_hashes_list, columns=["Original_Word", "Hash"])
+    word_hashes_df["Count"] = counts
+    word_hashes_df.sort_values(by = "Count", ascending=False, inplace=True)
 
     #If the user hasn't already picked thresholds, asks for them.
-    top_thresh, bottom_thresh = get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_numbers_df)
+    top_thresh, bottom_thresh = get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df)
 
-    #Shuffles the numbers
-    np.random.seed(seed)
-    word_numbers_df["Number"] = np.random.permutation(word_numbers_df["Number"])
 
-    # Retrieves the first word's number above the user count threshold (if it exists) and sets them all to it.
-    above_thresh_nums = list(word_numbers_df[(word_numbers_df.Count > top_thresh)].Number)
-    if len(above_thresh_nums) != 0:
-        above_thresh_num = above_thresh_nums[0]
-        word_numbers_df.loc[word_numbers_df.Count > top_thresh, 'Number'] = above_thresh_num
+    # Retrieves the first word's hash above the user count threshold (if it exists) and sets them all to it.
+    above_thresh_df = word_hashes_df[(word_hashes_df.Count > top_thresh)]
+    above_thresh_hashes = list(above_thresh_df.Hash)
+    if len(above_thresh_hashes) != 0:
+        above_thresh_hash = above_thresh_hashes[0]
+        word_hashes_df.loc[word_hashes_df.Count > top_thresh, 'Hash'] = above_thresh_hash
+    # Replaces the hashes in the dictionary word_hashes for those above the threshold.
+    for row in above_thresh_df.itertuples():
+        word_hashes[getattr(row, "Original_Word")][0] = getattr(row, "Hash")
 
 
     # The same process for the words below the bottom threshold
-    below_thresh_nums = list(word_numbers_df[(word_numbers_df.Count < bottom_thresh)].Number)
-    if len(below_thresh_nums) != 0:
-        below_thresh_num = below_thresh_nums[0]
-        word_numbers_df.loc[word_numbers_df.Count < bottom_thresh, 'Number'] = below_thresh_num
+    below_thresh_df = word_hashes_df[(word_hashes_df.Count < bottom_thresh)]
+    below_thresh_hashes = list(below_thresh_df.Hash)
+    if len(below_thresh_hashes) != 0:
+        below_thresh_hash = below_thresh_hashes[0]
+        word_hashes_df.loc[word_hashes_df.Count < bottom_thresh, 'Hash'] = below_thresh_hash
+    # Replaces the hashes in the dictionary word_hashes for those below the threshold.
+    for row in below_thresh_df.itertuples():
+        word_hashes[getattr(row, "Original_Word")][0] = getattr(row, "Hash")
 
-    # Refills the dictionary of {words -> [number, count]} with the shuffled numbers
-    for row in word_numbers_df.itertuples():
-        word_numbers[getattr(row, "Original_Word")][0] = getattr(row, "Number")
 
-    # Iterates back over the text, replacing the words with their shuffled numbers
-    obscured_text = assign_or_replace_text(original_text, punctuation, word_numbers, word_numbers_list, False, ps, stemming, case_sensitive)
+    # Iterates back over the text, replacing the words with their hashes
+    obscured_text = assign_or_replace_text(original_text, punctuation, word_hashes,
+                                            word_hashes_list, False, ps, stemming,
+                                            case_sensitive, salt, concat_hashes)
 
     csv_df["obscured_"+ column_name] = obscured_text
     if delete_orig:
@@ -260,10 +255,10 @@ def run_program(file_name, column_name, output_base, delete_orig, index_num, cas
             raise
     path = r"./obscured_" + file_name + r"/"
 
-    export_tables(csv_df, word_numbers_df, output_base, column_name, case_sensitive,
+    export_tables(csv_df, word_hashes_df, output_base, column_name, case_sensitive,
                     stemming, punctuation, top_thresh, bottom_thresh, index_num, path)
 
-    print(len(word_numbers_df), "unique words numeralized from", len(original_text),
+    print(len(word_hashes_df), "unique words numeralized from", len(original_text),
           "lines. Case Sensitive: " + str(case_sensitive) + ". Stemming: " + str(stemming) +
           ". Punctuation: "+ str(punctuation))
 
@@ -271,9 +266,9 @@ def run_program(file_name, column_name, output_base, delete_orig, index_num, cas
 
 if __name__ == "__main__":
     '''
-    Replaces the text of a given column in a csv file with randomized numbers,
+    Replaces the text of a given column in a csv file with hashes,
     exporting the original file with the given column removed and with the new
-    column added. Exports another csv file with the mapping of orignal words to numbers.
+    column added. Exports another csv file with the mapping of orignal words to hashes.
     '''
     config = configparser.ConfigParser()
     config.read('obscuritext_configs.cfg')
@@ -288,9 +283,26 @@ if __name__ == "__main__":
     case_sensitive = config['Processing']['case_sensitive'].lower()
     stemming = config['Processing']['stemming'].lower()
     punctuation = config['Processing']['remove_punctuation'].lower()
-    seed = int(config['Processing']['random_seed'])
+    salt = config['Processing']['salt_string']
+    concat_hashes = config['Processing']['concat_hashes']
     top_thresh = config['Processing']['combine_above']
     bottom_thresh =config['Processing']['combine_below']
+
+    if stemming:
+        from nltk.stem import PorterStemmer
+        ps = PorterStemmer()
+    else:
+        ps = None
+
+    try:
+        index_num = int(index_num)
+    except ValueError:
+        index_num = None
+
+    try:
+        concat_hashes = int(concat_hashes)
+    except ValueError:
+        concat_hashes = None
 
     options = [case_sensitive, stemming, punctuation]
 
@@ -314,4 +326,5 @@ if __name__ == "__main__":
     for case in case_sensitive:
         for stem in stemming:
             for punc in punctuation:
-                run_program(file_name, column_name, output_base, delete_orig, index_num, case, stem, punc, seed, top_thresh, bottom_thresh)
+                run_program(file_name, column_name, output_base, delete_orig, index_num,
+                case, stem, punc, salt, top_thresh, bottom_thresh, concat_hashes)
