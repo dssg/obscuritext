9613c89671486a5f5d46b1dd8e856881eaf0b671
jack-barbey <jack.barbey@gmail.com>
2018-01-19 14:20:17 -0600

    Substantive changes to way the hashes are assigned that allows for user inputing stop words, ensuring replicability of encodings

--- text_obscure.py
+++ text_obscure.py
@@ -4,59 +4,44 @@ import pandas as pd
 import os, errno
 import configparser, hashlib, base64
 
-def get_hashes(word_list, word_hashes, word_hashes_list, salt, concat_hashes, stop_words):
+def hash_word(word, salt, concat_hashes, stop_words, stop_word_hash, stop_above_hash,
+              stop_below_hash, stop_above_words, stop_below_words):
+    '''
+    Hashes the word with a custom salt. Hashes words in the three stop words sets to the same value.
+    '''
+    if word in stop_above_words:
+        return stop_above_hash
+    elif word in stop_below_words:
+        return stop_below_words
+    elif word in stop_words:
+        return stop_word_hash
+    else:
+        to_hash = word + salt
+
+    hasher = hashlib.sha1(to_hash.encode('utf-8'))
+    #Hashes using SHA1 then optionally truncates to prevent too long of hashes. Removes the padding at the end.
+    return base64.urlsafe_b64encode(hasher.digest()).decode("utf-8").replace("=", "")[:concat_hashes]
+
+def replace_words(entry_words, words_dict, salt, concat_hashes, stop_words):
     '''
     Given a list of words from a csv cell, returns a string of obscured words.
     INPUTS:
-        word_list (list): the original words
-        word_hashes (dict): contains parings of word -> hash
-        word_hashes_list (list of lists): contains [[word1, hash1], [word2, hash2] ...]
-        concat_hashes (int or None): the number of digits to concat the hash
-    OUTPUT:
-        return_string (string): the string of hashes to stand in for the original words
-    '''
-    for word in word_list:
-        if word not in word_hashes:
-            if word in stop_words:
-                to_hash = "stop word hash" + salt
-            else:
-                to_hash = word + salt
-            hasher = hashlib.sha1(to_hash.encode('utf-8'))
-            #Hashes using SHA1 then optionally truncates to prevent too long of hashes. Removes the padding at the end.
-            word_hash = base64.urlsafe_b64encode(hasher.digest()).decode("utf-8").replace("=", "")[:concat_hashes]
-
-            # Dictionary of {word -> [hash, count]}
-            word_hashes[word] = [word_hash, 1]
-            # List of lists with the form [[word, hash]]
-            word_hashes_list.append([word, word_hash])
-        else:
-            # Increases count by 1 if already in dictionary
-            word_hashes[word][1] += 1
-
-
-def replace_words(word_list, word_hashes):
-    '''
-    Given a list of words from a csv cell, returns a string of obscured words.
-    INPUTS:
-        word_list (list): the unhased words
-        word_hashes (dict): contains parings of word -> hash
-        word_hashes_list (list of lists): contains [[word1, hash1], [word2, hash2] ...]
-        bottom_thresh (int): only replace the word if count above this
-        top_thresh (int): only replace the word if count below this
+        entry_words (list): the unhased words
+        salt (string): the user's string for salting
     OUTPUT:
         return_string (string): the string of hashes to stand in for the original words
     '''
     replace_string = ""
-    for word in word_list:
-        # Retrieves the hash for that word
-        word_hash = word_hashes[word][0]
+    for word in entry_words:
+        word_hash = words_dict[word][0]
         replace_string += str(word_hash) + " "
 
     return replace_string
 
 
-def assign_or_replace_text(original_text, punctuation, word_hashes, word_hashes_list,
-                            assign, ps, stemming, case_sensitive, salt, concat_hashes, stop_words):
+def assign_or_replace_text(original_text, punctuation, words_dict, words_list,
+                            assign, ps, stemming, case_sensitive, salt, concat_hashes, stop_words,
+                          stop_word_hash, stop_above_hash, stop_below_hash, stop_above_words, stop_below_words):
     '''
     Loops through the original text. Depending on assign (bool), performs one of
     two options. Either assigns hashes to the unique words OR
@@ -64,15 +49,16 @@ def assign_or_replace_text(original_text, punctuation, word_hashes, word_hashes_
     INPUTS:
         original_text (Series): the column to be obscured
         punctuation (bool): if punctuation should be removed
-        word_hashes (dict): to be filled on first pass with word -> hash representative
+        words_dict (dict): to be filled on first pass with word -> hash representative
             later to be used to replace words with hashes
-        word_hashes_list (list of lists): same info as above
+        words_list (list of lists): contains all unique words_list
         assign (bool): tells whether to run to assign hashes or to retrieve hashes
         ps: PorterStemmer or none
         case_sensitive (bool): indicates whether to be case_sensitive
+        salt (string): the user's string for salting
 
     OUTPUTS:
-        Nothing on the first pass. Populates word_hashes and word_hashes_list
+        Nothing on the first pass. Populates words_dict and words_list
         obscured_text (List of strings): the text with the hashes
     '''
     # A regex pattern for replacing all of the punctuation in one pass
@@ -81,34 +67,42 @@ def assign_or_replace_text(original_text, punctuation, word_hashes, word_hashes_
     if not assign:
         obscured_text = [] #Only used in the second pass
 
-    for text in original_text:
-        if type(text) is not "str":
-            text = str(text)
+    for entry in original_text:
+        if type(entry) is not str:
+            text = str(entry)
 
         if punctuation:
-            post_punc = pattern.sub(" ", text)
+            post_punc = pattern.sub(" ", entry)
         else:
             # Isolates punctuation to be treated as an individual word
-            post_punc = pattern.sub(r' \1 ', text)
+            post_punc = pattern.sub(r' \1 ', entry)
 
         if case_sensitive:
-            words_list = post_punc.split()
+            entry_words = post_punc.split()
         else:
-            words_list = post_punc.lower().split()
+            entry_words = post_punc.lower().split()
 
         if stemming:
-            words_list = [ps.stem(i) for i in words_list]
+            entry_words = [ps.stem(i) for i in entry_words]
 
         if assign:
-            get_hashes(words_list, word_hashes, word_hashes_list, salt, concat_hashes, stop_words)
+            for word in entry_words:
+                if word not in words_dict:
+                    word_hash = hash_word(word, salt, concat_hashes, stop_words, stop_word_hash, stop_above_hash,
+                                          stop_below_hash, stop_above_words, stop_below_words)
+                    words_dict[word] = [word_hash, 1]
+                    words_list.append([word, word_hash])
+                else:
+                    # Increases count by 1 if already in dictionary
+                    words_dict[word][1] += 1
         else:
-            replace_string = replace_words(words_list, word_hashes)
+            replace_string = replace_words(entry_words, words_dict, salt, concat_hashes, stop_words)
             obscured_text.append(replace_string)
 
     if not assign:
         return obscured_text
 
-def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df):
+def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, words_df):
     '''
     If top_thresh or bottom_thresh was set to "ask" in the config file, shows the user the
     head or tail of the dataset and asks for the desired count.
@@ -123,7 +117,7 @@ def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df):
         top_thresh = None
     else:
         if top_thresh == "ask":
-            print(word_hashes_df.head(20),  "\n")
+            print(words_df.head(20),  "\n")
             top_thresh = input("Combine words with counts above: ")
         while True:
             try:
@@ -138,7 +132,7 @@ def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df):
         bottom_thresh = None
     else:
         if bottom_thresh == "ask":
-            print("\n", word_hashes_df.tail(20), "\n")
+            print("\n", words_df.tail(20), "\n")
             bottom_thresh = input("Combine words with counts below: ")
         while True:
             try:
@@ -150,12 +144,15 @@ def get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df):
 
     return top_thresh, bottom_thresh
 
-def export_tables(csv_df, word_hashes_df, output_base, column_name, case_sensitive,
-                    stemming, punctuation, top_thresh, bottom_thresh, index_num, path):
+def export_tables(csv_df, words_df, output_base, column_name, case_sensitive,
+                    stemming, punctuation, top_thresh, bottom_thresh, index_num, path,
+                    salt, stop_words, stop_above_words, stop_below_words):
     '''
     Strings together the options to create a descriptive filename.
     '''
     output_name = output_base + "_" + column_name
+
+    output_name += "_Salt=" + salt
 
     if case_sensitive:
         output_name += "_Case"
@@ -175,12 +172,22 @@ def export_tables(csv_df, word_hashes_df, output_base, column_name, case_sensiti
     output_name += "_Above" + str(top_thresh)
     output_name += "_Below" + str(bottom_thresh)
 
-
     csv_df.to_csv(path + output_name + ".csv", index= not type(index_num) is int)
-    word_hashes_df.to_csv(path + "mappings_" + output_name + ".csv", index=False)
+    words_df.to_csv(path + "mappings_" + output_name + ".csv", index=False)
+
+    with open(path + "stop_words_" + output_name + ".txt", "w") as text_file:
+        text_file.write("To encode new data with the same obscurations, copy and paste these into the configurations. \n\n")
+        text_file.write("Stop Words: \n")
+        text_file.write(" ".join(stop_words) + "\n\n\n")
+        text_file.write("Stop Above Words: \n")
+        text_file.write(" ".join(stop_above_words) + "\n\n\n")
+        text_file.write("Stop Words Above Threshold: \n")
+        text_file.write(" ".join(stop_below_words))
+
 
 def run_program(file_name, column_name, output_base, delete_orig, index_num, case_sensitive,
-                stemming, punctuation, salt, top_thresh, bottom_thresh, concat_hashes, stop_words):
+                stemming, punctuation, salt, top_thresh, bottom_thresh, concat_hashes, stop_words,
+                stop_above_words, stop_below_words):
     '''
     Given the configuration settings, runs the obscuration process. Can be called many times for different settings.
     Inputs:
@@ -200,10 +207,16 @@ def run_program(file_name, column_name, output_base, delete_orig, index_num, cas
         Nothing, but exports a two files: one with the obscured text and the other mapping original_word -> hash
     '''
     #Contains a dictionary of original word -> [hash of word, count]
-    word_hashes = {}
+    words_dict = {}
     #Contains the same information for export to csv (using list of lists for efficiency)
-    word_hashes_list = []
+    words_list = []
 
+    # Defines a hash for all the stop words, so that they don't all need to be hashed.
+    stop_word_hash = hash_word("stop words", salt, concat_hashes, stop_words, '', '', '', stop_above_words, stop_below_words)
+    stop_above_hash = hash_word("stop above", salt, concat_hashes, stop_words, '', '', '', stop_above_words, stop_below_words)
+    stop_below_hash = hash_word("stop below", salt, concat_hashes, stop_words, '', '', '', stop_above_words, stop_below_words)
+
+    # Reads our initial csv
     csv_df = pd.read_csv(file_name + ".csv", encoding = "ISO-8859-1", index_col = index_num)
     original_text = csv_df[column_name]
 
@@ -213,52 +226,58 @@ def run_program(file_name, column_name, output_base, delete_orig, index_num, cas
     else:
         stop_words = set(stop_words.lower().split())
 
-    #The first pass through assign_or_replace_text to assign hashes
-    assign_or_replace_text(original_text, punctuation, word_hashes, word_hashes_list, True,
-                            ps, stemming, case_sensitive, salt, concat_hashes, stop_words)
+    #The first pass through assign_or_replace_text to get all the unique words. Fills words_list and words_dict
+    assign_or_replace_text(original_text, punctuation, words_dict, words_list, True,
+                            ps, stemming, case_sensitive, salt, concat_hashes, stop_words,
+                            stop_word_hash, stop_above_hash, stop_below_hash, stop_above_words,
+                            stop_below_words)
 
-    #Fetches the counts of the words, in order, to be filled into the word_hashes_df created below
+
+    #Fetches the counts of the words, in order, to be filled into the words_df created below
     counts = []
-    for word, word_hash in word_hashes_list:
-        counts.append(word_hashes[word][1])
+    for word, word_hash in words_list:
+        counts.append(words_dict[word][1])
 
     #Assigns the proper counts, and sorts by them
-    word_hashes_df = pd.DataFrame(word_hashes_list, columns=["Original_Word", "Hash"])
-    word_hashes_df["Count"] = counts
-    word_hashes_df.sort_values(by = "Count", ascending=False, inplace=True)
+    words_df = pd.DataFrame(words_list, columns=["Original_Word", "Hash"])
+    words_df["Count"] = counts
+    words_df.sort_values(by = "Count", ascending=False, inplace=True)
 
-    #If the user hasn't already picked thresholds, asks for them.
-    top_thresh, bottom_thresh = get_top_and_bottom_thresholds(top_thresh, bottom_thresh, word_hashes_df)
+    #If the user hasn't already picked thresholds, asks for them. Converts to ints
+    top_thresh, bottom_thresh = get_top_and_bottom_thresholds(top_thresh, bottom_thresh, words_df)
 
-
-    # Retrieves the first word's hash above the user count threshold (if it exists) and sets them all to it.
+    # Creates a set for the words above the top threshold. When hashing, will hash these the same
     if top_thresh != None:
-        above_thresh_df = word_hashes_df[(word_hashes_df.Count > top_thresh)]
-        above_thresh_hashes = list(above_thresh_df.Hash)
-        if len(above_thresh_hashes) != 0:
-            above_thresh_hash = above_thresh_hashes[0]
-            word_hashes_df.loc[word_hashes_df.Count > top_thresh, 'Hash'] = above_thresh_hash
-        # Replaces the hashes in the dictionary word_hashes for those above the threshold.
+        words_df.loc[words_df.Count > top_thresh, 'Hash'] = stop_above_hash
+
+        above_thresh_df = words_df[(words_df.Count > top_thresh)]
+        # For exporting to a text file
+        stop_above_words = list(above_thresh_df.Original_Word)
+
+        # Refilling the words_dict with the new hashes
         for row in above_thresh_df.itertuples():
-            word_hashes[getattr(row, "Original_Word")][0] = getattr(row, "Hash")
+            words_dict[getattr(row, "Original_Word")][0] = getattr(row, "Hash")
 
-
-    # The same process for the words below the bottom threshold
+    # Creates a set for the words below the top threshold. When hashing, will hash these the same
     if bottom_thresh != None:
-        below_thresh_df = word_hashes_df[(word_hashes_df.Count < bottom_thresh)]
-        below_thresh_hashes = list(below_thresh_df.Hash)
-        if len(below_thresh_hashes) != 0:
-            below_thresh_hash = below_thresh_hashes[0]
-            word_hashes_df.loc[word_hashes_df.Count < bottom_thresh, 'Hash'] = below_thresh_hash
-        # Replaces the hashes in the dictionary word_hashes for those below the threshold.
+        words_df.loc[words_df.Count < bottom_thresh, 'Hash'] = stop_below_hash
+
+        below_thresh_df = words_df[(words_df.Count < bottom_thresh)]
+        # For exporting to a text file
+        stop_below_words = list(below_thresh_df.Original_Word)
+
+        # Refilling the words_dict with the new hashes
         for row in below_thresh_df.itertuples():
-            word_hashes[getattr(row, "Original_Word")][0] = getattr(row, "Hash")
+            words_dict[getattr(row, "Original_Word")][0] = getattr(row, "Hash")
 
 
     # Iterates back over the text, replacing the words with their hashes
-    obscured_text = assign_or_replace_text(original_text, punctuation, word_hashes,
-                                            word_hashes_list, False, ps, stemming,
-                                            case_sensitive, salt, concat_hashes, stop_words)
+    obscured_text = assign_or_replace_text(original_text, punctuation, words_dict,
+                                            words_list, False, ps, stemming,
+                                            case_sensitive, salt, concat_hashes, stop_words,
+                                            stop_word_hash, stop_above_hash, stop_below_hash,
+                                            stop_above_words, stop_below_words)
+
 
     csv_df["obscured_"+ column_name] = obscured_text
     if delete_orig:
@@ -271,14 +290,14 @@ def run_program(file_name, column_name, output_base, delete_orig, index_num, cas
             raise
     path = r"./obscured_" + file_name + r"/"
 
-    export_tables(csv_df, word_hashes_df, output_base, column_name, case_sensitive,
-                    stemming, punctuation, top_thresh, bottom_thresh, index_num, path)
+    export_tables(csv_df, words_df, output_base, column_name, case_sensitive,
+                    stemming, punctuation, top_thresh, bottom_thresh, index_num, path,
+                    salt, stop_words, stop_above_words, stop_below_words)
 
-    print(len(word_hashes_df), "unique words numeralized from", len(original_text),
+
+    print(len(words_df), "unique words numeralized from", len(original_text),
           "lines. Case Sensitive: " + str(case_sensitive) + ". Stemming: " + str(stemming) +
           ". Punctuation: "+ str(punctuation))
-
-
 
 if __name__ == "__main__":
     '''
@@ -304,6 +323,8 @@ if __name__ == "__main__":
     top_thresh = config['Processing']['combine_above'].lower()
     bottom_thresh = config['Processing']['combine_below'].lower()
     stop_words = config['Processing']['stop_words']
+    stop_above_words = config['Processing']['stop_above_words']
+    stop_below_words = config['Processing']['stop_below_words']
 
     if stemming:
         from nltk.stem import PorterStemmer
@@ -323,6 +344,11 @@ if __name__ == "__main__":
     except ValueError:
         concat_hashes = None
 
+    stop_above_words = set(stop_above_words.split())
+    stop_below_words = set(stop_below_words.split())
+
+    if (len(stop_above_words) != 0 or len(stop_above_words) != 0) and (top_thresh != "none" or bottom_thresh != "none"):
+        raise ValueError('If there are stop_above_words or stop_below_words present, the top and bottom thresholds must be set to None')
 
     # Runs the program with every combination of case_sensitive, stemming, and punctuation given by the user
     options = [case_sensitive, stemming, punctuation]
@@ -345,4 +371,5 @@ if __name__ == "__main__":
         for stem in stemming:
             for punc in punctuation:
                 run_program(file_name, column_name, output_base, delete_orig, index_num,
-                case, stem, punc, salt, top_thresh, bottom_thresh, concat_hashes, stop_words)
+                case, stem, punc, salt, top_thresh, bottom_thresh, concat_hashes, stop_words,
+                            stop_above_words, stop_below_words)
