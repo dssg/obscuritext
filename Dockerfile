ef32947042a34dc481f640380457ce97db04e334
jack-barbey <jack.barbey@gmail.com>
2018-01-31 13:16:22 -0600

    Pointing dockerfile to most up do date commit

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,9 @@ RUN apt-get update && \
                        python3-pip \
                        wget
 
-RUN wget -O- https://api.github.com/repos/dssg/obscuritext/tarball/b818152e99679fbb0830fe20223f1a9f29ea1d88 | \
+RUN wget -O- https://api.github.com/repos/dssg/obscuritext/tarball/7890e9a1bcb7b611b2ea0f00786237dfafa6b0fb | \
     tar -zxvf -
 
-WORKDIR /dssg-obscuritext-b818152
+WORKDIR /dssg-obscuritext-7890e9a
 
 RUN pip3 install -r requirements.txt
--- config_and_run/run_in_docker.sh
+++ config_and_run/run_in_docker.sh
@@ -7,10 +7,10 @@ CONFIG_NAME=obscuritext_configs
 
 # Copies csv file and config files into docker
 docker cp ${CSV_NAME}.csv \
-        ${CONTAINER_ID}:/dssg-obscuritext-b818152/${CSV_NAME}.csv
+        ${CONTAINER_ID}:/dssg-obscuritext-7890e9a/${CSV_NAME}.csv
 docker cp ${CONFIG_NAME}.cfg \
-        ${CONTAINER_ID}:/dssg-obscuritext-b818152/${CONFIG_NAME}.cfg
+        ${CONTAINER_ID}:/dssg-obscuritext-7890e9a/${CONFIG_NAME}.cfg
 
 docker exec -it ${CONTAINER_ID} sh -c "python3 text_obscure.py"
-docker cp ${CONTAINER_ID}:/dssg-obscuritext-b818152/obscured_${CSV_NAME} \
+docker cp ${CONTAINER_ID}:/dssg-obscuritext-7890e9a/obscured_${CSV_NAME} \
               ./obscured_${CSV_NAME}
